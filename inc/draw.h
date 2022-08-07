599904a104c14e6e711a22bc42b3a4f83e6d0de4
ioplkm <ioplkm352@gmail.com>
2022-08-07 05:13:52 +0500

    save

--- inc/draw.h
+++ inc/draw.h
@@ -2,11 +2,13 @@
 #define DRAW_H
 
 #include <pe/narrowcollision.h>
+#include <pe/collision.h>
 
 #include "cam.h"
 
 void drawPoint(uint32_t *b, Pixel p, uint32_t color);
 void drawLine(uint32_t *b, Pixel p1, Pixel p2, uint32_t color);
 void drawBox(uint32_t *b, Camera *pCam, CollisionBox *pCB, uint32_t color);
+void drawPolyhedra(uint32_t *b, Camera *pCam, ConvexPolyhedra *pP, uint32_t color);
 
 #endif
--- src/draw.c
+++ src/draw.c
@@ -1,6 +1,8 @@
 #include "../inc/draw.h"
 
 void drawPoint(uint32_t *b, Pixel p, uint32_t color) {
+  int pos = p.y * 1920 + p.x;
+  if (pos < 0 || pos > 1920 * 1080) return;
   b[p.y * 1920 + p.x] = color;
 }
 
@@ -18,7 +20,34 @@ void drawBox(uint32_t *b, Camera *pCam, CollisionBox *pCB, uint32_t color) {
     if (i & 1) v[i] = (Vector){v[i].x, v[i].y, -v[i].z};
     if (i & 2) v[i] = (Vector){v[i].x, -v[i].y, v[i].z};
     if (i & 4) v[i] = (Vector){-v[i].x, v[i].y, v[i].z};
-    v[i] = m34vMult(pCB->pRB->transformMatrix, v[i]);
+    v[i] = m34vMult(pCB->pRB->transform, v[i]);
+  }
+
+  Pixel p[8];
+  for (int i = 0; i < 8; i++)
+    p[i] = worldToScreen(pCam, v[i]);
+  drawLine(b, p[0], p[1], color);
+  drawLine(b, p[0], p[2], color);
+  drawLine(b, p[0], p[4], color);
+  drawLine(b, p[6], p[2], color);
+  drawLine(b, p[6], p[4], color);
+  drawLine(b, p[6], p[7], color);
+  drawLine(b, p[5], p[1], color);
+  drawLine(b, p[5], p[4], color);
+  drawLine(b, p[5], p[7], color);
+  drawLine(b, p[3], p[1], color);
+  drawLine(b, p[3], p[2], color);
+  drawLine(b, p[3], p[7], color);
+}
+
+void drawPolyhedra(uint32_t *b, Camera *pCam, ConvexPolyhedra *pCB, uint32_t color) {
+  Vector v[8]; //vertices of collisionBox
+  for (int i = 0; i < 8; i++) {
+    v[i] = pCB->halfSize;
+    if (i & 1) v[i] = (Vector){v[i].x, v[i].y, -v[i].z};
+    if (i & 2) v[i] = (Vector){v[i].x, -v[i].y, v[i].z};
+    if (i & 4) v[i] = (Vector){-v[i].x, v[i].y, v[i].z};
+    v[i] = m34vMult(pCB->pRB->transform, v[i]);
   }
 
   Pixel p[8];
